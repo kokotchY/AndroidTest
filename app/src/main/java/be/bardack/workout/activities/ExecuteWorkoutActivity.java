148285dad4f1743a28037cd32f8ed867adb56bec
Sebastien Canart <kokotchy@gmail.com>
2017-04-26 20:39:33 +0200

    Add ExecuteWorkoutActivity with debug content to ensure parceleable is working

--- app/src/main/java/be/bardack/workout/activities/ExecuteWorkoutActivity.java
+++ app/src/main/java/be/bardack/workout/activities/ExecuteWorkoutActivity.java
@@ -1,10 +1,41 @@
 package be.bardack.workout.activities;
 
-import android.app.Activity;
+import android.os.Bundle;
+import android.os.Parcelable;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import be.bardack.workout.R;
+import be.bardack.workout.model.ExerciseStep;
+import be.bardack.workout.model.Workout;
+import be.bardack.workout.model.WorkoutExercise;
 
 /**
  * Created by kokotchy on 4/26/17.
  */
 
-public class ExecuteWorkoutActivity extends Activity {
+public class ExecuteWorkoutActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_execute_workout);
+
+        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.debug_layout);
+        Workout workout = getIntent().getParcelableExtra("workout");
+        addTextView(linearLayout, workout.getTitle());
+        for (WorkoutExercise exercise : workout.getExercises()) {
+            addTextView(linearLayout, "Exercise: " + exercise.getTitle());
+            for (ExerciseStep step : exercise.getSteps()) {
+                addTextView(linearLayout, "Step: " + step.getName());
+            }
+        }
+    }
+
+    private void addTextView(LinearLayout layout, String text) {
+        TextView textView = new TextView(this);
+        textView.setText(text);
+        layout.addView(textView);
+    }
 }
